05c072a6c00d475fdb83906bca2505231b6e2dfe
Suryansh Satwaliya <suryanshsatwaliya@gmail.com>
2026-03-04 23:19:18 +0530

    port change

--- backend/app.py
+++ backend/app.py
@@ -13,8 +13,9 @@ socketio = SocketIO(app, cors_allowed_origins="*", async_mode="threading")
 
 # ── Email Config (Gmail SMTP) ─────────────────────────
 app.config["MAIL_SERVER"]         = "smtp.gmail.com"
-app.config["MAIL_PORT"]           = 587
-app.config["MAIL_USE_TLS"]        = True
+app.config["MAIL_PORT"]    = 465
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
 app.config["MAIL_USERNAME"]       = os.environ.get("MAIL_USERNAME", "suryansh.unofficial08@gmail.com")
 app.config["MAIL_PASSWORD"]       = os.environ.get("MAIL_PASSWORD", "ttfjngsnnncbjobc")
 app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_USERNAME", "suryansh.unofficial08@gmail.com")
